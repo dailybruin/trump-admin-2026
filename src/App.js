dda87107904bf5b67c3c27fd2a9d599825ba2eba
jessicaoyep <joyep@g.ucla.edu>
2026-02-12 16:45:17 -0800

    fix RightColumn to take defaultProps, tested w/ 2 iterations in App.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,32 @@ import RightColumn from './components/right-column';
 
 function App() {
   const [ data, setData ] = useState(null);
-  
+
+//test right column component with default props//
+  const defaultProps = {
+    map_description: "The highlight of my career is being able to bust their ass. December 2, 2006, best day of my college career.",
+
+    article_title_1: "Looking ahead to UCLA football’s 2026 season following lackluster 2025 campaign",
+    article_byline_1: "Connor Dullinger and Grant Walters",
+    article_url_1: "https://dailybruin.com/2025/11/23/looking-ahead-to-ucla-footballs-2026-season-following-lackluster-2025-campaign",
+    article_image_1: "https://assets3.dailybruin.com/images/rivalry-issue-25-26/A.sp_.football.DullysDrop.UCLAFuture.11.20.25.AGS_-2414ff54267f479b1d27c4ac52a51ec3.jpg",  
+
+    article_title_2: "Looking ahead to UCLA football’s 2026 season following lackluster 2025 campaign",
+    article_byline_2: "Connor Dullinger and Grant Walters",
+    article_url_2: "https://dailybruin.com/2025/11/23/looking-ahead-to-ucla-footballs-2026-season-following-lackluster-2025-campaign",
+    article_image_2: "https://assets3.dailybruin.com/images/rivalry-issue-25-26/A.sp_.football.DullysDrop.UCLAFuture.11.20.25.AGS_-2414ff54267f479b1d27c4ac52a51ec3.jpg" , 
+
+    article_title_3: "Looking ahead to UCLA football’s 2026 season following lackluster 2025 campaign",
+    article_byline_3: "Connor Dullinger and Grant Walters",
+    article_url_3: "https://dailybruin.com/2025/11/23/looking-ahead-to-ucla-footballs-2026-season-following-lackluster-2025-campaign" ,
+    article_image_3: "https://assets3.dailybruin.com/images/rivalry-issue-25-26/A.sp_.football.DullysDrop.UCLAFuture.11.20.25.AGS_-2414ff54267f479b1d27c4ac52a51ec3.jpg"  ,
+}
+
 return (
   <div>
-  <RightColumn
-        mapdescription = {"The highlight of my career is being able to bust their ass. December 2, 2006, best day of my college career."}
-        title = {"Looking ahead to UCLA football’s 2026 season following lackluster 2025 campaign"}
-         byline = {"Connor Dullinger and Grant Walters"}
-         url = {"https://dailybruin.com/2025/11/23/looking-ahead-to-ucla-footballs-2026-season-following-lackluster-2025-campaign"}
-         image = {"https://assets3.dailybruin.com/images/rivalry-issue-25-26/A.sp_.football.DullysDrop.UCLAFuture.11.20.25.AGS_-2414ff54267f479b1d27c4ac52a51ec3.jpg"}
-  />
-
-  <RightColumn
-        mapdescription = {"The highlight of my career is being able to bust their ass. December 2, 2006, best day of my college career."}
-        title = {"Looking ahead to UCLA football’s 2026 season following lackluster 2025 campaign"}
-         byline = {"Connor Dullinger and Grant Walters"}
-         url = {"https://dailybruin.com/2025/11/23/looking-ahead-to-ucla-footballs-2026-season-following-lackluster-2025-campaign"}
-         image = {"https://assets3.dailybruin.com/images/rivalry-issue-25-26/A.sp_.football.DullysDrop.UCLAFuture.11.20.25.AGS_-2414ff54267f479b1d27c4ac52a51ec3.jpg"}
-  />
+    //test two right columns for map section//
+  <RightColumn defaultProps={defaultProps}/>
+  <RightColumn defaultProps={defaultProps}/>
   </div>
 )
 
--- src/components/right-column.js
+++ src/components/right-column.js
@@ -12,31 +12,32 @@ function TextDescription ({mapdescription}) {
 }
 
 
-export default function RightColumn ({mapdescription, title, byline, url, image}) {
+/*need to integrate article.aml*/
+export default function RightColumn ({defaultProps}) {
     return (
         <div className="right-column-container">
             <TextDescription
-            mapdescription = {mapdescription}
+            mapdescription = {defaultProps.map_description}
             /> 
             <MapArticleCard
-            title = {title}
-            byline = {byline}
-            url = {url}
-            image = {image}
+            title = {defaultProps.article_title_1}
+            byline = {defaultProps.article_byline_1}
+            url = {defaultProps.article_url_1}
+            image = {defaultProps.article_image_1}
             />
 
             <MapArticleCard
-            title = {title}
-            byline = {byline}
-            url = {url}
-            image = {image}
+            title = {defaultProps.article_title_2}
+            byline = {defaultProps.article_byline_2}
+            url = {defaultProps.article_url_2}
+            image = {defaultProps.article_image_2}
             />
 
             <MapArticleCard
-            title = {title}
-            byline = {byline}
-            url = {url}
-            image = {image}
+            title = {defaultProps.article_title_3}
+            byline = {defaultProps.article_byline_3}
+            url = {defaultProps.article_url_3}
+            image = {defaultProps.article_image_3}
             />
         </div>
     );
