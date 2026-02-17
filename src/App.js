a737b059b9e0ac50ce2b79c6d2c391b77ecbb59e
jessicaoyep <joyep@g.ucla.edu>
2026-02-16 20:23:58 -0800

    Added Header for Map section (in App.js for now, will move to separate .jsx file for when full map section is assembled)

--- src/App.js
+++ src/App.js
@@ -30,41 +30,16 @@ function App() {
 //test two right columns for map section//
 return (
   <div>
+  
+  //MAP HEADER//
+  <h2 style = {{"color": "#165383", "textAlign": "center"}}>
+    Where budget cuts have impacted UCLA
+  </h2>
+
   <RightColumn defaultProps={defaultProps}/>
   <RightColumn defaultProps={defaultProps}/>
   </div>
 )
-
-  /*
-  //test map article card component //
-     return (
-      <div>
-         <TextDescription
-         mapdescription = {"The highlight of my career is being able to bust their ass. December 2, 2006, best day of my college career."}
-         />
-         <MapArticleCard
-         title = {"Looking ahead to UCLA football’s 2026 season following lackluster 2025 campaign"}
-         byline = {"Connor Dullinger and Grant Walters"}
-         url = {"https://dailybruin.com/2025/11/23/looking-ahead-to-ucla-footballs-2026-season-following-lackluster-2025-campaign"}
-         image = {"https://assets3.dailybruin.com/images/rivalry-issue-25-26/A.sp_.football.DullysDrop.UCLAFuture.11.20.25.AGS_-2414ff54267f479b1d27c4ac52a51ec3.jpg"}
-         />
- 
-         <MapArticleCard
-         title = {"Looking ahead to UCLA football’s 2026 season following lackluster 2025 campaign"}
-         byline = {"Connor Dullinger and Grant Walters"}
-         url = {"https://dailybruin.com/2025/11/23/looking-ahead-to-ucla-footballs-2026-season-following-lackluster-2025-campaign"}
-         image = {"https://assets3.dailybruin.com/images/rivalry-issue-25-26/A.sp_.football.DullysDrop.UCLAFuture.11.20.25.AGS_-2414ff54267f479b1d27c4ac52a51ec3.jpg"}
-         />
- 
-         <MapArticleCard
-         title = {"Looking ahead to UCLA football’s 2026 season following lackluster 2025 campaign"}
-         byline = {"Connor Dullinger and Grant Walters"}
-         url = {"https://dailybruin.com/2025/11/23/looking-ahead-to-ucla-footballs-2026-season-following-lackluster-2025-campaign"}
-         image = {"https://assets3.dailybruin.com/images/rivalry-issue-25-26/A.sp_.football.DullysDrop.UCLAFuture.11.20.25.AGS_-2414ff54267f479b1d27c4ac52a51ec3.jpg"}
-         />
-      </div>
-   )
-   */
 
   useEffect(() => {
 		fetch("<TODO: insert api url here>")
