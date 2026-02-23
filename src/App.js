d28365047a00490eb0ac14ef786dee1089ac4e3d
Kelly-Kim04 <kellyndu1@gmail.com>
2026-02-23 10:34:10 -0800

    Updated Subheader
    - Updated sub-header text and stylization
    - Added hyperlink

--- src/App.js
+++ src/App.js
@@ -8,8 +8,35 @@ import Footer from './components/Footer';
 
 function App() {
   const [ data, setData ] = useState({
-    headline:"\"A Perfect Storm\": How Budget Cuts Have Impacted UCLA",
-    subheading: `UCLA is generating an annual structural budget deficit approaching $425 million, Stephen Agostini, UCLA's Chief Financial Officer, told the Daily Bruin on Feb. 6. The university has not posted its annual <u>financial reports</u> – which Agostini said contain erroneous numbers – for the last two fiscal years as of early February. Chancellor Julio Frenk described several factors – some of which are laid out in the timeline below – as combining to create "a perfect storm" culminating in UCLA's current budget crisis. Programs across campus have also had their funding slashed, from student-run retention projects to tour guide organizations.\n\nThe Daily Bruin News team's special project – "A Perfect Storm": How Budget Cuts Have Impacted UCLA – outlines the history behind UCLA's budget shortfalls and tracks the parts of campus that have faced cuts.`
+  headline: <>"A Perfect Storm":<br/>How Budget Cuts Have Impacted UCLA</>,    
+  subheading: 
+    <>
+      UCLA is projected to run a $425 million deficit for the 2025-26 fiscal year, Stephen Agostini, 
+      UCLA's former chief financial officer, told the Daily Bruin on Feb. 6. Chancellor Julio Frenk 
+      announced that Agostini had been replaced as CFO four days after the Bruin published a Feb. 13 
+      article in which Agostini alleged that UCLA administrators have mismanaged the university's 
+      finances, contributing to the deficit. Mary Osako, vice chancellor for strategic communications, 
+      called Agostini's deficit projection an overestimate in a Feb. 17 statement but did not provide 
+      additional financial data.
+      <br /><br />
+      The university has not posted annual{" "}
+      
+      <a  href="https://www.finance.ucla.edu/corporate-accounting/ucla-annual-financial-reports"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <u>financial reports</u>
+      </a>
+      , which Agostini said contain erroneous numbers, for the last two fiscal years as of mid-February. 
+      Frenk told the Daily Bruin Jan. 15 that several factors – some of which are laid out in the 
+      timeline below – have combined to create "a perfect storm" culminating in UCLA's current budget 
+      crisis. Programs across campus have had their funding slashed, from student-run retention projects 
+      to tour guide organizations.
+      <br /><br />
+      The Daily Bruin News team's special project – "A Perfect Storm": How Budget Cuts Have Impacted 
+      UCLA – outlines the history behind UCLA's budget shortfalls and tracks the parts of campus that 
+      have faced cuts.
+    </>
   });
   
   useEffect(() => {
